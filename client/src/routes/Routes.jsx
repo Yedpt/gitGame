0fc7be3cd1242d53b2a4f9ca4230d42ad36a2e48
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-10-16 15:21:44 +0200

    structuring files

---
+++ client/src/routes/Routes.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {createBrowserRouter} from 'react-router-dom'
+import Layout from '../layout/Layout'
+import Home from '../pages/Home'
+
+export const routes = createBrowserRouter([{
+    path : '/',
+    element: <Layout/>,
+    children:[
+        {
+            index:true,
+            element: <Home/>
+        },
+        {
+            
+        }
+
+
+    ]
+}])
