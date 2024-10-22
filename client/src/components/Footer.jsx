de892224e52dc95a8107110e5bd94d649ab4a528
DarthVada36 <velazquez.vada@outlook.com>
2024-10-22 13:30:23 +0200

    Feature: Footer added 1.0

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import logo from "../img/gglogo1.svg";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="h-auto bg-greenDark flex flex-col md:flex-row justify-between items-center px-6 py-4 w-full">
+      {/* Logo a la izquierda */}
+      <div className="flex items-center md:justify-start justify-center w-full md:w-auto mb-4 md:mb-0">
+        <a href="/">
+          <img className="max-w-11 py-2" src={logo} alt="logo" />
+        </a>
+      </div>
 
-export default Footer
+      {/* Links centrados */}
+      <div className="flex justify-center space-x-6 text-light text-sm font-title font-normal mb-4 md:mb-0">
+        <a href="/" className="hover:text-greenLight">Noticias</a>
+        <a href="/" className="hover:text-greenLight">Memes</a>
+        <a href="/" className="hover:text-greenLight">Galería</a>
+        <a href="/" className="hover:text-greenLight">Contacto</a>
+      </div>
+
+      {/* Copyright centrado debajo de los links en responsive */}
+      <div className="text-light text-[11px] text-center leading-none font-paragraph font-extralight w-full md:w-auto">
+        <p>
+          Copyright © 2024 GitGame. <br />
+          Todos los derechos reservados.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
