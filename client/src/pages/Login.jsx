27314bf8c3323d708a9ec42e3b0b69328bf2f820
Yeder <ypimentel.tapia@gmail.com>
2024-10-23 10:37:52 +0200

    saving files

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,59 +20,66 @@ const Login = () => {
 
   return (
     <div
-      className="h-screen flex justify-center items-center px-4" // Agregamos px-4 para espacio en los laterales
+      className="min-h-screen flex flex-col justify-between px-8"  // Flex para centrar con espacios
       style={{
         backgroundImage: `url('/fondoLoginMobile.svg')`, // Asegúrate de colocar correctamente la ruta del archivo SVG
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
     >
-      <div className="bg-green-900 p-8 rounded-lg shadow-lg max-w-md w-full md:max-w-lg h-[80vh] md:h-[80vh]"> {/* Altura ajustada */}
-        <h2 className="text-white text-3xl mb-6 text-center">Inicia Sesión</h2>
-        {error && <p className="text-red-500 mb-4">{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-white text-sm mb-2" htmlFor="email">
-              CORREO
-            </label>
-            <input
-              className="w-full p-2 rounded bg-gray-100 text-gray-900"
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+      
+      
+      {/* Contenedor de login centrado */}
+      <div className="flex-grow flex justify-center items-center">
+        <div className="bg-green-900 p-8 rounded-lg shadow-lg max-w-md w-full md:max-w-lg">
+          <h2 className="text-white text-3xl mb-6 text-center">Inicia Sesión</h2>
+          {error && <p className="text-red-500 mb-4">{error}</p>}
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label className="block text-white text-sm mb-2" htmlFor="email">
+                CORREO
+              </label>
+              <input
+                className="w-full p-2 rounded bg-gray-100 text-gray-900"
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-white text-sm mb-2" htmlFor="password">
+                CONTRASEÑA
+              </label>
+              <input
+                className="w-full p-2 rounded bg-gray-100 text-gray-900"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button
+              className="w-full bg-green-500 hover:bg-green-600 text-white p-2 rounded"
+              type="submit"
+            >
+              Entrar
+            </button>
+          </form>
+          <div className="mt-4 text-center">
+            <p className="text-white text-sm">
+              ¿Aún no tienes cuenta? <a href="#" className="text-green-400">Crea una.</a>
+            </p>
+            <p className="text-white text-sm mt-2">
+              <a href="#" className="text-green-400">Olvidé mi contraseña</a>
+            </p>
           </div>
-          <div className="mb-6">
-            <label className="block text-white text-sm mb-2" htmlFor="password">
-              CONTRASEÑA
-            </label>
-            <input
-              className="w-full p-2 rounded bg-gray-100 text-gray-900"
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button
-            className="w-full bg-green-500 hover:bg-green-600 text-white p-2 rounded"
-            type="submit"
-          >
-            Entrar
-          </button>
-        </form>
-        <div className="mt-4 text-center">
-          <p className="text-white text-sm">
-            ¿Aún no tienes cuenta? <a href="#" className="text-green-400">Crea una.</a>
-          </p>
-          <p className="text-white text-sm mt-2">
-            <a href="#" className="text-green-400">Olvidé mi contraseña</a>
-          </p>
         </div>
       </div>
+      
+   
     </div>
   );
 };
