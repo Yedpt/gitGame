a2ae0a3407700592d7628c2e3fda5f9dfb5d1b36
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-10-24 23:53:47 +0200

    successful connection to the db

--- server/app.ts
+++ server/app.ts
@@ -1,1 +1,12 @@
-//app
+import connectionDb from "./database/conectionDb";
+
+async function main() {
+  try {
+    await connectionDb.authenticate();
+    console.log('Conexión exitosa.');
+  } catch (error) {
+    console.error('conexion fallida', error);
+  }
+}
+
+main();
--- server/database/conectionDb.ts
+++ server/database/conectionDb.ts
@@ -1,1 +1,12 @@
-//Database
+import { Sequelize } from "sequelize";
+
+
+const connectionDb = new Sequelize('book_app', 'root', '12345', {
+    host: 'localhost',
+    dialect: 'mysql',
+    define: {
+        timestamps: false
+    }
+  });
+
+export default connectionDb;
