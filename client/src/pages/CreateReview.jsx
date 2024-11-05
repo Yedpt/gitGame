53ebf20bba2f3b1ca55a0fdadee711019dbe4f53
Yeder <ypimentel.tapia@gmail.com>
2024-11-05 17:58:30 +0100

    multer conection done , images are saved in the reviews folder

--- client/src/pages/CreateReview.jsx
+++ client/src/pages/CreateReview.jsx
@@ -26,7 +26,7 @@ const CreateReview = () => {
     }
   }, [isLoggedIn, navigate]);
 
-const handleChange = (e) => {
+  const handleChange = (e) => {
     const { name, value, files } = e.target;
     setFormData({
       ...formData,
@@ -57,6 +57,7 @@ const handleChange = (e) => {
       formDataToSend.append('author', formData.author);
       formDataToSend.append('rating', formData.rating);
       formDataToSend.append('image_url', formData.image_url); // Usa el image_url del estado
+      formDataToSend.append('type', 'Reviews'); // Asegúrate de que el tipo sea 'Reviews'
 
       try {
         await createReview(formDataToSend);
@@ -69,91 +70,90 @@ const handleChange = (e) => {
     }
   };
 
-    
-return (
-  <div className="min-h-screen flex flex-col justify-between px-8 bg-cover bg-center bg-no-repeat py-12"
-      style={{ backgroundImage: `url(${pattern})` }}
-  >
-    <div className="flex-grow flex justify-center items-center mt-20">
-        <div className="bg-greenMid p-8 rounded-lg shadow-lg max-w-md w-full md:max-w-lg">
-          <h2 className="font-title text-white text-4xl mb-6 text-center">CREA UNA RESEÑA</h2>
+  return (
+    <div className="min-h-screen flex flex-col justify-between px-8 bg-cover bg-center bg-no-repeat py-12"
+        style={{ backgroundImage: `url(${pattern})` }}
+    >
+      <div className="flex-grow flex justify-center items-center mt-20">
+          <div className="bg-greenMid p-8 rounded-lg shadow-lg max-w-md w-full md:max-w-lg">
+            <h2 className="font-title text-white text-4xl mb-6 text-center">CREA UNA RESEÑA</h2>
 
-          <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
 
-            {/* title */}
-            <div className="mb-4">
-              <label className="font-title block text-greenLight text-lg mb-2">TÍTULO</label>
-              <input
-                type="text"
-                name="title"
-                value={formData.title}
-                onChange={handleChange}
-                className="w-full p-2 rounded-md bg-gray-100 text-gray-900 font-paragraph"
-              />
-              {errors.title && <p className="text-red-500 text-sm">{errors.title}</p>}
-            </div>
-
-            {/* Review */}
-            <div className="mb-4">
-              <label className="font-title block text-greenLight text-lg mb-2">RESEÑA</label>
-              <textarea
-                name="review"
-                value={formData.review}
-                onChange={handleChange}
-                className="w-full p-2 rounded-md bg-gray-100 text-gray-900 font-paragraph"
-              ></textarea>
-              {errors.review && <p className="text-red-500 text-sm">{errors.review}</p>}
-            </div>
-
-            {/* author */}
-            <div className="mb-4">
-                <label className="font-title block text-greenLight text-lg mb-2">AUTOR</label>
+              {/* title */}
+              <div className="mb-4">
+                <label className="font-title block text-greenLight text-lg mb-2">TÍTULO</label>
                 <input
-                type="text"
-                name="author"
-                value={formData.author}
-                onChange={handleChange}
-                className="w-full p-2 rounded-md bg-gray-100 text-gray-900 font-paragraph"
+                  type="text"
+                  name="title"
+                  value={formData.title}
+                  onChange={handleChange}
+                  className="w-full p-2 rounded-md bg-gray-100 text-gray-900 font-paragraph"
                 />
-                {errors.author && <p className="text-red-500 text-sm">{errors.author}</p>}
-            </div>
+                {errors.title && <p className="text-red-500 text-sm">{errors.title}</p>}
+              </div>
 
-            {/* rating */}
-          {user?.rol === 'admin' && (
-            <div className="mb-4">
-                <label className="font-title block text-greenLight text-lg mb-2">CALIFICACIÓN</label>
+              {/* Review */}
+              <div className="mb-4">
+                <label className="font-title block text-greenLight text-lg mb-2">RESEÑA</label>
+                <textarea
+                  name="review"
+                  value={formData.review}
+                  onChange={handleChange}
+                  className="w-full p-2 rounded-md bg-gray-100 text-gray-900 font-paragraph"
+                ></textarea>
+                {errors.review && <p className="text-red-500 text-sm">{errors.review}</p>}
+              </div>
+
+              {/* author */}
+              <div className="mb-4">
+                  <label className="font-title block text-greenLight text-lg mb-2">AUTOR</label>
+                  <input
+                    type="text"
+                    name="author"
+                    value={formData.author}
+                    onChange={handleChange}
+                    className="w-full p-2 rounded-md bg-gray-100 text-gray-900 font-paragraph"
+                  />
+                  {errors.author && <p className="text-red-500 text-sm">{errors.author}</p>}
+              </div>
+
+              {/* rating */}
+              {user?.rol === 'admin' && (
+                <div className="mb-4">
+                    <label className="font-title block text-greenLight text-lg mb-2">CALIFICACIÓN</label>
+                    <input
+                      type="number"
+                      name="rating"
+                      value={formData.rating}
+                      onChange={handleChange}
+                      className="w-full p-2 rounded-md bg-gray-100 text-gray-900 font-paragraph"
+                    />
+                    {errors.rating && <p className="text-red-500 text-sm">{errors.rating}</p>}
+                </div>
+              )}
+
+              {/* Image */}
+              <div className="mb-4">
+                <label className="font-title block text-greenLight text-lg mb-2">IMAGEN</label>
                 <input
-                type="number"
-                name="rating"
-                value={formData.rating}
-                onChange={handleChange}
-                className="w-full p-2 rounded-md bg-gray-100 text-gray-900 font-paragraph"
+                  type="file"
+                  name="image_url"
+                  onChange={handleChange}
+                  className="w-full p-2 rounded-md bg-gray-100 text-gray-900 font-paragraph"
                 />
-                {errors.rating && <p className="text-red-500 text-sm">{errors.rating}</p>}
-            </div>
-          )}
-          
-            {/* Image */}
-            <div className="mb-4">
-              <label className="font-title block text-greenLight text-lg mb-2">IMAGEN</label>
-              <input
-                type="file"
-                name="image_url"
-                onChange={handleChange}
-                className="w-full p-2 rounded-md bg-gray-100 text-gray-900 font-paragraph"
-              />
-              {errors.image_url && <p className="text-red-500 text-sm">{errors.image_url}</p>}
-            </div>
+                {errors.image_url && <p className="text-red-500 text-sm">{errors.image_url}</p>}
+              </div>
 
-            <div className="mb-4 flex justify-center">
-            <button type="submit" className="font-title w-32 bg-greenLight hover:bg-greenMidsec text-white p-3 rounded-md">
-                Enviar
-              </button>
-            </div>
-          </form>
+              <div className="mb-4 flex justify-center">
+                <button type="submit" className="font-title w-32 bg-greenLight hover:bg-greenMidsec text-white p-3 rounded-md">
+                    Enviar
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
