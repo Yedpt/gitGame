1a3534ccdb8ca2df2c8e5b8011fe1e57327cd70d
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-11-10 15:07:03 +0100

    incorporating authenticateToken

--- server/routes/newsRoutes.ts
+++ server/routes/newsRoutes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { getAllNews, createNew, deleteNew, updateNew, getNewById } from "../controllers/newsController";
 import { upload } from "../middleware/newUpload";
+import { authenticateToken, isAdmin } from "../middleware/userRole";
 
 export const newRouter = Router();
 
 newRouter.get('/', getAllNews);
 newRouter.get('/:id', getNewById);
-newRouter.post('/', upload.fields([
+newRouter.post('/', authenticateToken, isAdmin, upload.fields([
   { name: 'image_url', maxCount: 1 },
   { name: 'image2_url', maxCount: 1 }
 ]), createNew);
-newRouter.delete('/:id', deleteNew)
-newRouter.put('/:id', updateNew)
+newRouter.delete('/:id', authenticateToken, isAdmin, deleteNew)
+newRouter.put('/:id', authenticateToken, isAdmin, updateNew)
